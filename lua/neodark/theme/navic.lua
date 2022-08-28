3a833e3ce7bde3fc1d7a3c59ee922586eae911ce
aymenhafeez <aymennh@gmail.com>
2022-08-29 00:47:20 +0100

    added navic highlighting for winbar

--- lua/neodark/theme/navic.lua
+++ lua/neodark/theme/navic.lua
@@ -2,20 +2,20 @@ local navic = {}
 
 function navic.setup(palette)
   local navic_ = {
-    NavicIconsClass = { fg = palette.yellow3 },
+    NavicIconsClass = { fg = palette.bufline_yellow3 },
     NavicIconsVariable = { fg = palette.blue },
     NavicIconsModule = { fg = palette.blue },
-    NavicIconsMethod = { fg = palette.dark_purple },
-    NavicIconsField = { fg = palette.dark_purple },
+    NavicIconsMethod = { fg = palette.bufline_dark_purple },
+    NavicIconsField = { fg = palette.bufline_dark_purple },
     NavicIconsFunction = { fg = palette.blue },
     NavicIconsProperty = { fg = palette.blue, bold = false},
-    NavicIconsEnum = { fg = palette.dark_purple },
+    NavicIconsEnum = { fg = palette.bufline_dark_purple },
     NavicIconsEnumMember = { fg = palette.yellow },
     NavicIconsTypeParamter = { fg = palette.yellow },
     NavicIconsConstant = { fg = palette.green },
     NavicIconsStruct = { fg = palette.blue },
-    NavicText = { fg = palette.light_grey2, italic = false, bold = false },
-    NavicSeparator = { fg = palette.nord_blue},
+    NavicText = { fg = palette.bufline_light_grey2, italic = false, bold = false },
+    NavicSeparator = { fg = palette.bufline_nord_blue},
   }
 
   return navic_
