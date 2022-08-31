87528d21bf4b9db9bf524caa7bbdde929b48a275
aymenhafeez <aymennh@gmail.com>
2022-08-31 04:07:42 +0100

    cmp changes

--- lua/neodark/theme/cmp.lua
+++ lua/neodark/theme/cmp.lua
@@ -8,12 +8,12 @@ function cmp.setup(palette)
     CmpItemAbbrMatchFuzzyDefault = { fg = palette.cyan, gui =' bold' },
     CmpItemKind = { fg = palette.blue },
     CmpItemAbbr = { fg = palette.fg },
-    CmpItemMenuDefault = { fg = palette.blue },
-    CmpItemKindFieldDefault = { fg = palette.bufline_dark_purple },
-    CmpItemKindSnippetDefault = { fg = palette.bufline_yellow3 },
-    CmpItemKindTextDefault = { fg = palette.bufline_yellow3 },
-    CmpItemKindFunctionDefault = { fg = palette.bufline_dark_purple },
-    CmpItemKindVariableDefault = { fg = palette.blue }
+    CmpItemMenu = { fg = palette.blue },
+    CmpItemKindField = { fg = palette.bufline_dark_purple },
+    CmpItemKindSnippet = { fg = palette.bufline_yellow3 },
+    CmpItemKindText = { fg = palette.bufline_yellow3 },
+    CmpItemKindFunction = { fg = palette.bufline_dark_purple },
+    CmpItemKindVariable = { fg = palette.blue }
   }
 
   return cmp_
